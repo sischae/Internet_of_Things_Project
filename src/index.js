c155142f0e5db4ee5337cda94ac4ba608cb5536b
sischae <test@test.com>
2021-09-20 22:24:53 +0300

    Logging of user log-in
    - Users can log-in using username and password
    - Logging in or reconnecting after more than 30 minutes will add a log-in event to the database table log_users (timestamp INT, user TEXT)
    - The landing page generates a list of all log-in events
    - Logging out will tell the server to reset the users timestamp in the database to enable the server to detect a new log-in
    - Added style setup for displaying the list of log-in data
    - Added functionality to index.js to fetch and generate elements to display the list of log-in data
    
    - Added settings.ejs and settings.js files with basic layout (header with title and menu)

--- src/index.js
+++ src/index.js
@@ -19,6 +19,79 @@ app.set('view engine', 'ejs');
 
 // return landing page
 app.get('/', async (req, res, next) => {
+    auth_user(req, res, next, 'index');
+});
+
+
+// return settings page
+app.get('/settings', async (req, res, next) => {
+    auth_user(req, res, next, 'settings');
+});
+
+
+// return logout page
+app.get('/logout', async (req, res) => {
+    res.render('logout');
+});
+
+
+// log out
+app.get('/req_logout', async (req, res, next) => {
+    auth_user(req, res, next, 'req_logout');
+});
+
+
+// return list of log in activity
+app.get('/activity', async (req, res, next) => {
+    auth_user(req, res, next, 'activity');
+});
+
+
+
+
+
+// FUNCTIONS
+
+// get a list of all log in activity
+function get_activity(req, res, next) {
+    let log = JSON.parse('[]');
+    
+    // open database
+    let db = new sqlite3.Database('./data/data.db', sqlite3.OPEN_READWRITE, (err) => {                      // connect to database
+        if (err) {                                                                                          // catch errors
+            return console.error(err.message);
+        }
+    });
+
+    // fetch all entries
+    db.serialize(() => {
+        db.each(`SELECT timestamp as timestamp, user as user FROM log_users`, (err, row) => {               // read all entries from table
+        if (err) {                                                                                          // catch errors
+            console.error(err.message);                                                                     // log
+        }
+        
+        let time = new Date(row.timestamp)
+        let time_formatted = time.toLocaleString();
+        log.push({"timestamp":time_formatted,"user":row.user.charAt(0).toUpperCase() + row.user.slice(1)});
+        });
+    });
+
+    // close the database connection
+    db.close((err) => {                                                                                     // close database connection
+        if (err) {                                                                                          // catch errors
+            res.status(500).send('Internal Error');                                                         // send error information to client
+            return console.error(err.message);                                                                  // log
+        }
+        
+        res.status(200).send(JSON.stringify(log.reverse()));                                                    // reverse array to show latest activities first
+        next();
+    });
+}
+
+
+
+// authenticate user an redirect to correct page
+function auth_user(req, res, next, redirect) {
     var authheader = req.headers.authorization;
     res.setHeader('WWW-Authenticate', 'Basic');
  
@@ -36,47 +109,96 @@ app.get('/', async (req, res, next) => {
         
         // generate and compare hashes
         crypto.pbkdf2(password, username, 100000, 64, 'sha512', (err, derivedKey) => {
-            if (err) throw err;                                                                             // catch errors
+            if (err) throw err;                                                                                 // catch errors
             
-            let hash_in = derivedKey.toString('hex');                                                       // generate hash for given username and password
-            let hash_db = 'init';                                                                           // stores hash from database
-            
+            let hash_in = derivedKey.toString('hex');                                                           // generate hash for given username and password
+            let hash_db = 'init';                                                                               // stores hash from database
+            let last_login = 0;
+
             
             // open database
-            let db = new sqlite3.Database('./data/data.db', sqlite3.OPEN_READWRITE, (err) => {              // connect to database
-                if (err) {                                                                                  // catch errors
-                    return console.error(err.message);                                                      // log
+            let db = new sqlite3.Database('./data/data.db', sqlite3.OPEN_READWRITE, (err) => {                  // connect to database
+                if (err) {                                                                                      // catch errors
+                    return console.error(err.message);                                                          // log
                 }
             });
 
             // fetch all users
             db.serialize(() => {
-                db.each(`SELECT username as username, hash as hash FROM users`, (err, row) => { // read all cars from database
-                    if (err) {                                                                              // catch errors
-                        console.error(err.message);                                                         // log
+                db.each(`SELECT username as username, hash as hash, timestamp as timestamp FROM users`, (err, row) => {
+                    if (err) {                                                                                  // catch errors
+                        console.error(err.message);                                                             // log
                     }
                     
                     // filter username
-                    if(row.username == username) {                                                          // filter users
-                        hash_db = row.hash;                                                                 // get hash from database
+                    if(row.username == username) {                                                              // filter users
+                        hash_db = row.hash;                                                                     // get hash from database
+                        last_login = row.timestamp;
                     }
                 });
             });
 
             // close the database connection
-            db.close((err) => {                                                                             // close database connection
-                if (err) {                                                                                  // catch errors
-                    return console.error(err.message);                                                      // log
+            db.close((err) => {                                                                                 // close database connection
+                if (err) {                                                                                      // catch errors
+                    return console.error(err.message);                                                          // log
                 }
                 
                 
                 // compare generated hash with hash from database
-                if (hash_in == hash_db) {
-                    // If Authorized user
+                if (hash_in == hash_db) {                                                                       // If Authorized user
+                    // log activity
+                    let db = new sqlite3.Database('./data/data.db');                                            // connect to database
+                    
+                    // update timestamp of last activity
+                    let time = Date.now();
+                    
+                    if((time - last_login) > 30 * 60 * 1000) {                                                  // if users last login was more than 30 minutes ago -> new log in
+                        console.log('new login');
+                        
+                        db.run('INSERT INTO log_users(timestamp, user) VALUES("' + Date.now() + '", "' + username + '")', function(err) {
+                            if (err) {                                                                          // catch errors
+                                return console.log(err.message);                                                // log
+                            }
+                        });
+                    }
+                    
+                    db.run('UPDATE users SET timestamp = ' + time + ' WHERE username = "' + username + '";', function(err) {
+                        if (err) {                                                                              // catch errors
+                            return console.log(err.message);                                                    // log
+                        }
+                    });
+    
+                    
+                    db.close();                                                                                 // close database connection
+                    
+                    
                     res.setHeader('WWW-Authenticate', 'Basic');
-                    res.render('index', {username: username.charAt(0).toUpperCase() + username.slice(1)});  // make first letter uppercase
-                    next();
-                } else {                                                                                    // invalid login
+                    switch(redirect) {
+                            case 'activity':
+                                get_activity(req, res, next);
+                                break;
+                            
+                            case 'req_logout':
+                                let db = new sqlite3.Database('./data/data.db');                                // connect to database
+                            
+                                // reset timestamp to indicate log out
+                                db.run('UPDATE users SET timestamp = 0 WHERE username = "' + username + '";', function(err) {
+                                    if (err) {                                                                              // catch errors
+                                        return console.log(err.message);                                                    // log
+                                    }
+                                });
+                            
+                                db.close();                                                                                 // close database connection
+                                res.status(200).send('OK');
+                                break;
+                            
+                            default:
+                                res.render(redirect, {username: username.charAt(0).toUpperCase() + username.slice(1)});     // make first letter uppercase
+                                next();
+                                break;
+                    }
+                } else {                                                                                        // invalid login
                     var err = new Error('You are not authenticated!');
                     err.status = 401;
                     return next(err);
@@ -84,14 +206,7 @@ app.get('/', async (req, res, next) => {
             });
           });
     }
-});
-
-
-// return logout page
-app.get('/logout', async (req, res) => {
-    res.render('logout');
-});
-
+}
 
 
 
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,13 +1,57 @@
 'use strict';
 
+/******************************************************************************************
+LOGOUT
+******************************************************************************************/
+
 document.getElementById("logout").addEventListener("click", function() {
-    // send invalid request
-    var XHR = new XMLHttpRequest();
-    XHR.open("GET", "/logout", true, "invalid", "invalid");
-    XHR.send();
+    // send request to officially log out
+    var logout_request = new XMLHttpRequest();
+    logout_request.open( "GET", '/req_logout', false );
+    logout_request.send( null );
+    
+    // send invalid request to log out
+    var logout = new XMLHttpRequest();
+    logout.open("GET", "/logout", true, "invalid", "invalid");
+    logout.send();
 
     // forward user to logout page
     setTimeout(function () {
         window.location.href = "/logout";
-    }, 100);
+    }, 10);
 });
+
+
+
+
+/******************************************************************************************
+LOGIN ACTIVITY
+******************************************************************************************/
+
+let log = JSON.parse('[]');                                                                             // stores all cars returned from the server
+
+// fetch all activities
+fetch('/activity', {                                                                                    // send get request to fetch all cars in the system
+    method: 'get',
+}).then(res => res.text()).then(res => {
+    log = JSON.parse(res);                                                                              // parse returned json object to cars
+    
+    // add a flex box for each activity
+    for (let i in log) {
+      addLog(log[i].timestamp, log[i].user);                                                            // create a new flex box and add it to the main page
+    }
+});
+
+
+// create a new flex box and add it to the main page
+function addLog(timestamp, user) {
+    const div = document.createElement('div');                                                          // create new div
+
+    div.className = 'flex_log_data';                                                                    // set div class
+    
+    // add all information about the car as innerHTML of the created div
+    div.innerHTML = timestamp + ` : `+ user;
+    
+    document.getElementById('log_users').appendChild(div);                                               // finally add the created div to the main page
+}
+
