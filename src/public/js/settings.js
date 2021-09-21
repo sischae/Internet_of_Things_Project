d123060ec61ed6035f006b8b06d3ec1c5bea3dc1
sischae <test@test.com>
2021-09-21 21:33:52 +0300

    Added multipage log-in history
    The list of the log-in activities will still only display eight elements at once.
    Now, older events can be displayed by switching between multiple pages.
    Two buttons allow the user to navigate between the pages. An indicator tells the user what page is being displayed at the moment.

--- src/public/js/settings.js
+++ src/public/js/settings.js
@@ -34,46 +34,136 @@ LOGIN ACTIVITY
 ******************************************************************************************/
 
 let log = JSON.parse('[]');                                                                             // stores all cars returned from the server
+let list_length = 8;
+let pages = 0;
+let cur_page = 0;
+let last_amm = 0;
 
 // fetch all activities
 fetch('/activity', {                                                                                    // send get request to fetch all cars in the system
     method: 'get',
 }).then(res => res.text()).then(res => {
     log = JSON.parse(res);                                                                              // parse returned json object to cars
+
+    let count = log.length;                                                                             // get length of received array
+    pages = Math.ceil(count/list_length);                                                               // calculate how many pages of the given lenght can be displayed
+    last_amm = count % list_length;                                                                     // calculate how many elements can be displayed on the last page
     
-    
-    let count = log.length;
-    let list_length = 8;
-    if(count < 8) list_length = count;
-    
-    // add a flex box for each activity
-    for (let i = 0; i < list_length; i++) {
-        addLog(log[i].timestamp, log[i].user);                                                          // create a new flex box and add it to the page
+    if(last_amm == 0) {                                                                                 // prevent generating an empty page
+        pages = pages - 1;
     }
     
-    // fill list with placeholder items to keep same size
-    if(list_length < 8) {
-        for (let i = 0; i < (8 - list_length); i++) {
-            addLog('&nbsp;', '');                                                                       // create a new spaceholder and add it to the list
-        }
-    }
-    
-    // add information about data access
-    const info = document.createElement('div');                                                         // create new div
-    info.innerHTML = `Only admins can see all users log-in activity.`;
-    info.className = 'info'; 
-    document.getElementById('log_users').appendChild(info);                                             // finally add the created div to the main page
+    loadPage('');                                                                                       // load default page (page 0)
 });
 
 
+
+// load other page of log-in activities
+function loadPage(dir) {
+    // clear the hole list
+    document.getElementById('log_users').innerHTML = '';
+    
+    // add title
+    const title = document.createElement('div');                                                        // create title
+    title.innerHTML = `
+                    <h3>Log in activity</h3>
+    `;
+    title.style = "margin-bottom: 0px;";                                                                // reduce margin-bottom
+    title.className = "flex_log_data";                                                                  // set class
+    document.getElementById('log_users').appendChild(title);                                            // add title
+    
+    
+    // check if button was pressed and update cur_page if allowed
+    if(dir == 'last' && cur_page != 0) {
+        cur_page = cur_page - 1;                                                                        // generate last page
+    } else if(dir == 'next' && cur_page < (pages - 1)) {
+        cur_page = cur_page + 1;                                                                        // generate next page
+    }
+    
+    
+    if(cur_page < pages - 1 || ((cur_page == pages - 1) && last_amm == 0)) {                            // if the page to be displayed is full of elements
+        // add a flex box for each activity
+        for (let i = 0; i < list_length; i++) {
+            addLog(log[(cur_page * 8) + i].timestamp, log[(cur_page * 8) + i].user, i);                 // create a new flex box and add it to the page
+        }
+    } else {                                                                                            // if the page to be displayed is the last page and has less than list_length elements
+        // add a flex box for each activity
+        let i = 0;
+        for (i = 0; i < last_amm; i++) {
+            addLog(log[(cur_page * 8) + i].timestamp, log[(cur_page * 8) + i].user, i);                 // create a new flex box and add it to the page
+        }
+        while(i < 8) {
+            addLog('&nbsp;', '', i);                                                                    // create a new spaceholder and add it to the list
+            i++;
+        }
+    }
+    
+    
+    // add navigation
+    const arrows = document.createElement('div');                                                       // create buttons to navigate between the pages
+    arrows.innerHTML = `
+            <button id="lastPage" class="page_nav" style="margin-left: 30px;"><--</button>
+            <button id="nextPage" class="page_nav">--></button>
+            <label class="info" style="margin-left: 0px;">` + (cur_page + 1) + `/` + pages + `</label>
+    `;
+    document.getElementById('log_users').appendChild(arrows);                                           // add buttons
+    
+    // add event listeners for both buttons
+    document.getElementById("lastPage").addEventListener("click", function(){
+        loadPage('last');
+    });
+    document.getElementById("nextPage").addEventListener("click", function() {
+        loadPage('next')
+    });
+    
+    
+    // enable/disable buttons depending on the current page
+    if(cur_page == 0) {                                                                                 // first page
+        document.getElementById("lastPage").disabled = true;
+        if(pages == 1) {                                                                                // only one page available
+            document.getElementById("nextPage").disabled = true;
+        } else {
+            document.getElementById("nextPage").disabled = false;
+        }
+    } else if(cur_page == (pages - 1)) {                                                                // last page available
+        document.getElementById("nextPage").disabled = true;
+        if(cur_page == 0) {                                                                             // not the first page at the same time
+            document.getElementById("lastPage").disabled = true;
+        } else {
+            document.getElementById("lastPage").disabled = false;
+        }
+    } else {                                                                                            // not the first && not the last page available
+        document.getElementById("lastPage").disabled = false;
+        document.getElementById("nextPage").disabled = false;
+    }
+    
+    
+    // add information about data access
+    const info = document.createElement('div');                                                         // create info
+    info.innerHTML = `
+        <div>
+            <label>Only admins can see all users log-in activity.</label>
+        </div>
+    `;
+    info.className = 'info';                                                                            // set class
+    document.getElementById('log_users').appendChild(info);                                             // add info
+}
+
+
+
 // create a new flex box and add it to the main page
-function addLog(timestamp, user) {
+function addLog(timestamp, user, idx) {
     const div = document.createElement('div');                                                          // create new div
 
     div.className = 'flex_log_data';                                                                    // set div class
     
     // add all information about the car as innerHTML of the created div
     div.innerHTML = timestamp + user;
+    
+    // change margin-bottom on last element
+    if(idx == list_length - 1) {
+        div.style = "margin-bottom: 10px;";
+    }
     
     document.getElementById('log_users').appendChild(div);                                              // finally add the created div to the main page
 }
