22175b95f9f335ac0bdc49508cb2af3c36a9af91
sischae <80777165+sischae@users.noreply.github.com>
2021-10-23 14:24:04 +0300

    Documentation 3. 3.1, 3.2 + Minor adjustments to help page
    Help page:
    - Added link to user manual
    - Moved script for accordion effect to the help.js file

--- src/public/js/help.js
+++ src/public/js/help.js
@@ -79,3 +79,23 @@ ws_client.onopen = () => {
     };
 };
 
+
+
+
+/******************************************************************************************
+ACCORDION
+******************************************************************************************/
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+    panel.style.maxHeight = null;
+    } else {
+    panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+});
+}
