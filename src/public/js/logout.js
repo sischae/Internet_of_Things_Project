83e4f57888dc34b81fdfcd835cf80482532e68ff
sischae <80777165+sischae@users.noreply.github.com>
2021-10-25 15:00:57 +0300

    Changed full authentication method to session based authentication
    As Basic HTTP Authentication is no longer completely supportet by Chromium based browsers, logging out would not work reliably. Therefore, the whole auhtentication system has been changed.
    Now, session based authentication is used!
    
    Added a custom login form to the logout page.

--- src/public/js/logout.js
+++ src/public/js/logout.js
@@ -1,11 +1,6 @@
 'use strict';
 
 
-/******************************************************************************************
-LOGOUT PAGE
-******************************************************************************************/
-
-var logout_button_back = document.getElementById("logout_button_back");
-var logout_button_next = document.getElementById("logout_button_next");
-
-
+if(document.getElementById("label_error").innerHTML != '') {
+    document.getElementById("spacer").style.height = "44px";
+}
